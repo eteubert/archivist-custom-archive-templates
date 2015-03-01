d117b970eb31d495ed9692718394ed67aa80a2b2
Eric Teubert <eric@ericteubert.de>
2015-03-01 19:13:01 +0800

    fix flattr and homepage

--- archivist.php
+++ archivist.php
@@ -573,24 +573,11 @@ if ( ! class_exists( 'archivist' ) ) {
 							<p>
 								<?php _e( 'Hey, I\'m Eric. I created this plugin.<br/> If you like it, consider to flattr me a beer.', archivist::get_textdomain() ); ?>
 							</p>
-							<script type="text/javascript">
-							/* <![CDATA[ */
-							    (function() {
-							        var s = document.createElement('script'), t = document.getElementsByTagName('script')[0];
-							        s.type = 'text/javascript';
-							        s.async = true;
-							        s.src = 'http://api.flattr.com/js/0.6/load.js?mode=auto';
-							        t.parentNode.insertBefore(s, t);
-							    })();
-							/* ]]> */
-							</script>
 							<p>
-								<a class="FlattrButton" style="display:none;" rev="flattr;button:compact;" href="http://www.FarBeyondProgramming.com/wordpress/plugin-archivist-custom-archive"></a>
-								<noscript><a href="http://flattr.com/thing/396382/WordPress-Plugin-Archivist-Custom-Archive-Templates" target="_blank">
-								<img src="http://api.flattr.com/button/flattr-badge-large.png" alt="Flattr this" title="Flattr this" border="0" /></a></noscript>
+								<script id='fb61vb1'>(function(i){var f,s=document.getElementById(i);f=document.createElement('iframe');f.src='//api.flattr.com/button/view/?uid=ericteubert&button=compact&url=https%3A%2F%2Fflattr.com%2Fprofile%2Fericteubert';f.title='Flattr';f.height=20;f.width=110;f.style.borderWidth=0;s.parentNode.insertBefore(f,s);})('fb61vb1');</script>
 							</p>
 							<p>
-								<?php echo wp_sprintf( __( 'Get in touch: Visit my <a href="%1s">Homepage</a>, follow me on <a href="%2s">Twitter</a> or look at my projects on <a href="%3s">GitHub</a>.', archivist::get_textdomain() ), 'http://www.FarBeyondProgramming.com/', 'http://www.twitter.com/ericteubert', 'https://github.com/eteubert' ) ?>
+								<?php echo wp_sprintf( __( 'Get in touch: Visit my <a href="%1s">Homepage</a>, follow me on <a href="%2s">Twitter</a> or look at my projects on <a href="%3s">GitHub</a>.', archivist::get_textdomain() ), 'http://www.ericteubert.de/', 'http://www.twitter.com/ericteubert', 'https://github.com/eteubert' ) ?>
 							</p>
 						</div>
 					</div>
--- readme.txt
+++ readme.txt
@@ -1,6 +1,6 @@
 === Archivist - Custom Archive Templates ===
 Contributors: eteubert
-Donate link: http://www.FarBeyondProgramming.com/wordpress/plugin-archivist-custom-archive
+Donate link: http://flattr.com/thing/396382/WordPress-Plugin-Archivist-Custom-Archive-Templates
 Tags: archive, loop, shortcode, category, tag, custom, query, template, html, customizable
 Requires at least: 3.0
 Tested up to: 3.5
